31b34d19e27d9d1718b1306e441fd4a00b778c39
muntisa <muntisa@gmail.com>
2014-04-11 09:09:27 +0000

    Added bibo:doi, short_name to cheminf.owl; adapted Python script

--- documentation/scripts/CheminfData2Rdf.py
+++ documentation/scripts/CheminfData2Rdf.py
@@ -3,7 +3,7 @@
 # by Cristian R Munteanu (muntisa@gmail.com)
 
 # the header fields could be at random positions
-# the script will search only for 'ShortName', 'CHEMINF_URI', 'Label', 'subClassOf', 'Description', 'Comment'
+# the script will search only for 'ShortName', 'CHEMINF_URI', 'Label', 'subClassOf', 'Description', 'DOI', 'Comment'
 # the minimum should be: 'CHEMINF_URI', 'Label', 'subClassOf', 'Description'
 
 ChemDataFile="CheminfData.txt" # file with data to be converted
@@ -13,7 +13,7 @@ f.close()
 
 header=lines[0]
 headerL=(header.replace('\n', '')).split("\t") # get the header as list
-# ex: ['ShortName', 'CHEMINF_URI', 'Label', 'subClassOf', 'subClassName', 'Description', 'Comment', 'Note']
+# ex: ['ShortName', 'CHEMINF_URI', 'Label', 'subClassOf', 'subClassName', 'Description', 'DOI', 'Comment', 'Note']
 
 for i in range(1,len(lines)): # for each descriptor get the info
     descriptorL=((lines[i]).replace('\n', '')).split("\t") # get descriptor fields as list
@@ -26,7 +26,9 @@ for i in range(1,len(lines)): # for each descriptor get the info
         sSubClass=""
         sDescription=""
         sComment=""
+        sDOI=""
 
+        # ADD strip! remove spaces! Replace new lines etc with nothong!
         for j in range(len(headerL)): # for each header
             if headerL[j]=="ShortName":
                 sShortName=descriptorL[j]
@@ -43,16 +45,25 @@ for i in range(1,len(lines)): # for each descriptor get the info
                             if headerL[j]=="Description":
                                 sDescription=descriptorL[j]
                             else:
-                                if headerL[j]=="Comment":
-                                    sComment=descriptorL[j]
+                                if headerL[j]=="DOI":
+                                    sDOI=descriptorL[j]
+                                else:
+                                    if headerL[j]=="Comment":
+                                        sComment=descriptorL[j]
+                                    
 
         # write RDF
         if sCHEMINF_URI!="" and sLabel!="" and sSubClass!="" and sDescription!="": # only if find a minimum description
             sRDF+="    <!-- http://semanticscience.org/resource/"+sCHEMINF_URI+" -->\n\n"
             sRDF+="    <owl:Class rdf:about=\"&resource;"+sCHEMINF_URI+"\">\n"
-            sRDF+="        <rdfs:label>"+sLabel+"</rdfs:label>\n"
+            sRDF+="        <rdfs:label xml:lang=\"en\">"+sLabel+"</rdfs:label>\n"   
             sRDF+="        <rdfs:subClassOf rdf:resource=\"&resource;"+sSubClass+"\"/>\n"
             sRDF+="        <dc:description>"+sDescription+"</dc:description>\n"
+            #sRDF+="        <obo:IAO_0000115>"+sDescription+"</obo:IAO_0000115>\n"
+
+            if sDOI!="":
+                sRDF+="        <bibo:doi rdf:datatype=\"&xsd;string\">"+sDOI+"</bibo:doi>\n"
+                
             if sComment!="":
                 sRDF+="        <rdfs:comment>"+sComment+"</rdfs:comment>\n"
             if sShortName!="":
